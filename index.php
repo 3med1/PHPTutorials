098e273a9b04bd1ce9da5bdd542269da6b6483f9
hussienalrubaye <hussien89aa@yahoo.com>
2016-03-12 13:27:53 -0500

    varibles

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+ 
+ <?php
+
+echo "hello, PHP student";
+echo "<h1> This List of Sessions</h1>";
+
+ ?>
+
+  </br>
+ 
+<UL>
+<li><a href="varibles.php">varibles</a> </li>
+</UL>
+
+</body>
+</html>
