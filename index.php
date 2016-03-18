b8e4d20b8061e63d9b6d9961600b79efc078f514
hussien alrubaye <hussien89aa@yahoo.com>
2016-03-18 09:16:44 -0400

    array funcations

--- index.php
+++ index.php
@@ -14,8 +14,12 @@ echo "<h1> This List of Sessions</h1>";
 
   </br>
  
+ 
 <UL>
 <li><a href="varibles.php">varibles</a> </li>
+<li><a href="operations.php">operations</a> </li>
+<li><a href="strings.php">strings</a> </li>
+<li><a href="array.php">array</a> </li>
 </UL>
 
 </body>
