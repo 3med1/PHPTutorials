a5d97cf5c82ae5b724eac1021db7f31fd94f791d
hussien alrubaye <hussien89aa@yahoo.com>
2016-03-20 03:47:56 -0400

    php  forms

---
+++ control.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+ <h1>Welcome to control panel</h1>
+
+<h1>POST Results</h1>
+<?php
+
+echo print_r($_POST);
+
+?>
+</br>
+Usernmae:<?php echo $_POST['username'];?>
+</br>
+Password:<?php echo $_POST['password'];?>
+</br>
+
+<h1>GET Results</h1>
+<?php
+
+echo print_r($_GET);
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -20,6 +20,12 @@ echo "<h1> This List of Sessions</h1>";
 <li><a href="operations.php">operations</a> </li>
 <li><a href="strings.php">strings</a> </li>
 <li><a href="array.php">array</a> </li>
+<li><a href="functions_arry.php">arry function</a> </li>
+<li><a href="null.php">Null and Boolean</a> </li>
+<li><a href="cast.php">Casting</a> </li>
+<li><a href="condation.php">IF-Statement</a> </li>
+<li><a href="functions.php">functions</a> </li>
+<li><a href="functions_multi.php">multiple value functions</a> </li>
 </UL>
 
 </body>
